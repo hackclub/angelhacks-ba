1ee6450d744770980918a2ba09a0835df3fe704b
ryanchou-dev <ryanchou111@gmail.com>
2023-04-21 03:27:07 -0700

    branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,41 +14,86 @@ export default function Home() {
         className="w-5/12 opacity-70 absolute right-0 -mb-4 bottom-0 "
         alt="bridge"
       />
-      <div className=" bg-gradient-to-b -z-20 from-[#7eecff] to-[#defaff]  h-screen ">
+      <div className=" bg-gradient-to-b -z-20 from-[#8eddfd] to-[#a4d3f3]  h-screen ">
         <div
           className={
-            "z-0 pt-12 md:pl-12 flex justify-center items-center md:justify-start md:items-start "
+            "z-0  md:pl-12 flex justify-center items-center md:justify-start md:items-start "
           }
         >
           <div className="md:text-left text-center">
-            <div className="text-center mb-24 -rotate-12 mt-24 bg-[#009aab] bg-opacity-70 p-4 rounded-lg border-dashed w-fit">
-              {/* "mt-24 bg-[#009aab] bg-opacity-70 p-4 rounded-lg border-dashed w-fit" */}
+            <div className="text-center  mt-16 p-4 rounded-lg border-dashed w-fit">
+              <div className="mb-6 flex justify-center items-center">
+                <Image src={Logo} alt="angelhacks logo" className="w-1/4 " />
+              </div>
               <span
                 className={
                   nunito.className +
-                  " text-6xl sm:text-7xl font-extrabold text-[#fdb32b] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                  " text-6xl sm:text-7xl font-extrabold text-[#0291a0]"
                 }
               >
-                AngelHacks!
+                Angel
               </span>
+              <span
+                className={
+                  nunito.className +
+                  " text-6xl sm:text-7xl font-extrabold text-[#fdb32b]"
+                }
+              >
+                Hacks
+              </span>
+              <span
+                className={
+                  nunito.className +
+                  " text-6xl sm:text-7xl font-extrabold text-[#0291a0]"
+                }
+              >
+                {" "}
+                3.0
+              </span>
+
               <p
                 className={
                   nunito.className +
-                  " font-extrabold mt-2 text-[#ffcfb9] text-4xl sm:text-5xl"
+                  " font-extrabold uppercase mt-2 text-[#de865e] text-4xl sm:text-5xl"
                 }
               >
-                Bay Area Satelite
+                Bay Area
               </p>
             </div>
-            <a
-              href="https://google.com"
+
+            <p className={nunito.className + " mt-8  font-semibold text-2xl "}>
+              May 27-28, 2023, 9AM-9PM <br />
+              <span className="text-xl">Address</span>
+            </p>
+            <p
               className={
-                nunito.className +
-                " bg-[#fdb32b] p-4 text-2xl rounded-lg text-white font-semibold mt-12 sm:mt-16"
+                nunito.className + " mt-8 mb-12  font-semibold text-2xl "
               }
             >
-              Register
-            </a>
+              Design, create, and ship your own game!
+            </p>
+            <div className="group hover:cursor-pointer">
+              <div className="group-hover:translate-y-1 transition ease-in-out duration-300">
+                <a
+                  className={
+                    nunito.className +
+                    "font-extrabold text-4xl -ml-2 px-24 py-2 relative bg-black rounded-xl  border-black border-2"
+                  }
+                >
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </a>
+              </div>
+              <div className="-mt-12">
+                <a
+                  className={
+                    nunito.className +
+                    " font-extrabold relative text-4xl px-24 py-2 bg-[#ffb400] rounded-xl z-20  border-black border-2"
+                  }
+                >
+                  Join us!
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         <div className={"absolute bottom-0  w-full overflow-hidden"}>
