df84182f7eb8279861c4aaa9e0628667e6033360
ryanchou-dev <ryanchou111@gmail.com>
2023-04-21 04:24:02 -0700

    redesign

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const nunito = Nunito({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={"min-h-screen pb-12 bg-black"}>
+    <main className={"min-h-screen pb-12 bg-white"}>
       <Image
         src={Bridge}
         className="w-5/12 opacity-70 absolute right-0 -mb-4 bottom-0 "
@@ -17,39 +17,24 @@ export default function Home() {
       <div className=" bg-gradient-to-b -z-20 from-[#8eddfd] to-[#a4d3f3]  h-screen ">
         <div
           className={
-            "z-0  md:pl-12 flex justify-center items-center md:justify-start md:items-start "
+            "z-0 pt-12  sm:pl-12 flex justify-center items-center sm:justify-start sm:items-start "
           }
         >
-          <div className="md:text-left text-center">
-            <div className="text-center  mt-16 p-4 rounded-lg border-dashed w-fit">
-              <div className="mb-6 flex justify-center items-center">
+          <div className="sm:text-left text-center p-4 lg:px-16">
+            <div>
+              <div className="mb-6 flex justify-center items-center sm:justify-start sm:items-start">
                 <Image src={Logo} alt="angelhacks logo" className="w-1/4 " />
               </div>
-              <span
+
+              <p
                 className={
                   nunito.className +
-                  " text-6xl sm:text-7xl font-extrabold text-[#0291a0]"
+                  " text-[#0291a0] text-6xl sm:text-7xl font-extrabold"
                 }
               >
                 Angel
-              </span>
-              <span
-                className={
-                  nunito.className +
-                  " text-6xl sm:text-7xl font-extrabold text-[#fdb32b]"
-                }
-              >
-                Hacks
-              </span>
-              <span
-                className={
-                  nunito.className +
-                  " text-6xl sm:text-7xl font-extrabold text-[#0291a0]"
-                }
-              >
-                {" "}
-                3.0
-              </span>
+                <span className={"text-[#fdb32b]"}>Hacks</span> 3.0
+              </p>
 
               <p
                 className={
@@ -66,39 +51,48 @@ export default function Home() {
               <span className="text-xl">Address</span>
             </p>
             <p
-              className={
-                nunito.className + " mt-8 mb-12  font-semibold text-2xl "
-              }
+              className={nunito.className + " mt-8 mb-12  font-bold text-3xl "}
             >
               Design, create, and ship your own game!
             </p>
-            <div className="group hover:cursor-pointer">
-              <div className="group-hover:translate-y-1 transition ease-in-out duration-300">
-                <a
-                  className={
-                    nunito.className +
-                    "font-extrabold text-4xl -ml-2 px-24 py-2 relative bg-black rounded-xl  border-black border-2"
-                  }
-                >
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                </a>
-              </div>
-              <div className="-mt-12">
-                <a
-                  className={
-                    nunito.className +
-                    " font-extrabold relative text-4xl px-24 py-2 bg-[#ffb400] rounded-xl z-20  border-black border-2"
-                  }
-                >
-                  Join us!
-                </a>
+            <div className="flex justify-center items-center sm:justify-start sm:items-start">
+              <div className="group hover:cursor-pointer ">
+                <div className="group-hover:translate-y-1 transition ease-in-out duration-300">
+                  <a
+                    className={
+                      nunito.className +
+                      "font-extrabold text-4xl -ml-2 px-24 py-2 relative bg-black rounded-xl  border-black border-2"
+                    }
+                  >
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  </a>
+                </div>
+                <div className="-mt-12">
+                  <a
+                    className={
+                      nunito.className +
+                      " font-extrabold relative text-4xl px-24 py-2 bg-[#ffb400] rounded-xl z-20  border-black border-2"
+                    }
+                  >
+                    Join us!
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className={"absolute bottom-0  w-full overflow-hidden"}>
-          <Image src={Clouds} alt="logo" priority className="max-w-screen " />
+        <div className={"absolute bottom-0  overflow-hidden"}>
+          <Image src={Clouds} alt="logo" priority className="w-screen " />
         </div>
+      </div>
+      <div className="text-center mt-16">
+        <p
+          className={
+            nunito.className + " text-6xl font-extrabold text-[#ffb400]"
+          }
+        >
+          It's a Game Jam!
+        </p>
       </div>
     </main>
   );
